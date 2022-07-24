48e5801cbaa3fd4ec2f51002401ee2c77977fc88
B Suresh <sursssh@gmail.com>
2022-07-24 20:37:46 +0530

    Final_Commit

--- app/src/main/java/com/my/sqliteactivity/MainActivity.kt
+++ app/src/main/java/com/my/sqliteactivity/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var empArrayId:Array<String>
     private lateinit var empArrayName:Array<String>
     private lateinit var empArrayEmail:Array<String>
+    private lateinit var adapter:RecyclerViewAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -46,6 +47,7 @@ class MainActivity : AppCompatActivity() {
                 u_id.text.clear()
                 u_name.text.clear()
                 u_email.text.clear()
+                viewRecord()
             }
         }else{
             Toast.makeText(applicationContext,"id or name or email cannot be blank",Toast.LENGTH_LONG).show()
@@ -71,7 +73,7 @@ class MainActivity : AppCompatActivity() {
         }
         //creating custom ArrayAdapter
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
-        val adapter = RecyclerViewAdapter(this,empArrayId,empArrayName,empArrayEmail)
+        adapter = RecyclerViewAdapter(this,empArrayId,empArrayName,empArrayEmail)
         adapter.notifyDataSetChanged()
         recyclerView.adapter = adapter
 
@@ -105,13 +107,14 @@ class MainActivity : AppCompatActivity() {
             }else{
                 Toast.makeText(applicationContext,"id or name or email cannot be blank",Toast.LENGTH_LONG).show()
             }
-
+            viewRecord()
         })
         dialogBuilder.setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, which ->
             //pass
         })
         val b = dialogBuilder.create()
         b.show()
+
     }
     //method for deleting records based on id
     private fun deleteRecord(){
@@ -134,17 +137,19 @@ class MainActivity : AppCompatActivity() {
                 val status = databaseHandler.deleteEmployee(EmpModelClass(Integer.parseInt(deleteId),"",""))
                 if(status > -1){
                     Toast.makeText(applicationContext,"record deleted", Toast.LENGTH_LONG).show()
+
                 }
             }else{
                 Toast.makeText(applicationContext,"id or name or email cannot be blank",Toast.LENGTH_LONG).show()
             }
-
+            viewRecord()
         })
         dialogBuilder.setNegativeButton("Cancel", DialogInterface.OnClickListener { _, _ ->
             //pass
         })
         val b = dialogBuilder.create()
         b.show()
+
     }
 
 }
